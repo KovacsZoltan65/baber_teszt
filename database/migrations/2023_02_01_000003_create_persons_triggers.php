f4bc930846b8cd5321fe20c6fb451bb987a38c9c
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2023-02-03 08:53:58 +0100

    adatbázis

--- database/migrations/2023_02_01_000003_create_persons_triggers.php
+++ database/migrations/2023_02_01_000003_create_persons_triggers.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        // "INSERT" művelet LOG -olása
         $procedure = "DROP TRIGGER IF EXISTS baber_teszt.persons_AFTER_INSERT;";
         $procedure .= "CREATE DEFINER=`root`@`localhost` TRIGGER baber_teszt.persons_AFTER_INSERT AFTER INSERT ON persons 
             FOR EACH ROW
@@ -26,21 +27,47 @@ return new class extends Migration
                 END IF;
             END;";
         
+        // "UPDATE" művelet LOG -olása
         $procedure .= "DROP TRIGGER IF EXISTS baber_teszt.persons_AFTER_UPDATE;";
-        $procedure .= "CREATE DEFINER='root'@'localhost' TRIGGER baber_teszt.persons_AFTER_UPDATE AFTER UPDATE ON persons
+        $procedure .= "CREATE DEFINER=`root`@`localhost` TRIGGER baber_teszt.persons_AFTER_UPDATE AFTER UPDATE ON persons
             FOR EACH ROW
             BEGIN
                 # Kell futtatni a triggert?
                 IF @disable_trigger IS NULL OR @disable_trigger = 0 THEN
+                    
+                    # Változtatás alap állapota
                     SET @mod_op = 'U';
-                    IF NEW.deleted_at IS NOT NULL THEN
-                    SET @mod_op = 'D';
+
+                    # Ha 'soft delete' művelet történt, akkor...
+                    IF OLD.deleted_at IS NULL AND NEW.deleted_at IS NOT NULL THEN
+                      SET @mod_op = 'SD';
+                    END IF;
+
+                    # Ha 'visszaállítás' művelet történt, akkor...
+                    IF OLD.deleted_at IS NOT NULL AND NEW.deleted_at IS NULL THEN
+                      SET @mod_op = 'R';
                     END IF;
 
                     INSERT INTO baber_teszt.persons_logs 
-                    SELECT NULL, LOCALTIMESTAMP, @mod_op, p.* FROM persons p WHERE p.id = NEW.id;
+                        SELECT NULL, LOCALTIMESTAMP, @mod_op, p.* FROM persons p WHERE p.id = NEW.id;
+                    
                 END IF;
             END;";
+        
+        // "DELETE" művelet LOG -olása
+        $procedure .= "DROP TRIGGER IF EXISTS baber_teszt.persons_BEFORE_DELETE;";
+        $procedure .= "CREATE DEFINER=`root`@`localhost` TRIGGER baber_teszt.persons_BEFORE_DELETE BEFORE DELETE ON persons 
+            FOR EACH ROW
+            BEGIN
+                # Kell futtatni a triggert?
+                IF @disable_trigger IS NULL OR @disable_trigger = 0 THEN
+
+                    INSERT INTO baber_teszt.persons_logs 
+                        SELECT NULL, LOCALTIMESTAMP, 'D', p.* FROM persons p WHERE p.id = OLD.id;
+
+                END IF;
+            END;";
+        
         DB::unprepared($procedure);
     }
 
--- database/migrations/2023_02_01_000006_create_files_triggers.php
+++ database/migrations/2023_02_01_000006_create_files_triggers.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        // "INSERT" művelet LOG -olása
         $procedure = "DROP TRIGGER IF EXISTS baber_teszt.files_AFTER_INSERT;";
         $procedure .= "CREATE DEFINER=`root`@`localhost` TRIGGER baber_teszt.files_AFTER_INSERT AFTER INSERT ON files 
             FOR EACH ROW
@@ -26,6 +27,7 @@ return new class extends Migration
                 END IF;
             END;";
         
+        // "UPDATE" művelet LOG -olása
         $procedure .= "DROP TRIGGER IF EXISTS baber_teszt.files_AFTER_UPDATE;";
         $procedure .= "CREATE DEFINER='root'@'localhost' TRIGGER baber_teszt.files_AFTER_UPDATE AFTER UPDATE ON files
             FOR EACH ROW
@@ -41,6 +43,21 @@ return new class extends Migration
                     SELECT NULL, LOCALTIMESTAMP, @mod_op, f.* FROM files f WHERE f.id = NEW.id;
                 END IF;
             END;";
+        
+        // "DELETE" művelet LOG -olása
+        $procedure .= "DROP TRIGGER IF EXISTS baber_teszt.files_BEFORE_DELETE;";
+        $procedure .= "CREATE DEFINER=`root`@`localhost` TRIGGER baber_teszt.files_BEFORE_DELETE BEFORE DELETE ON files 
+            FOR EACH ROW
+            BEGIN
+                # Kell futtatni a triggert?
+                IF @disable_trigger IS NULL OR @disable_trigger = 0 THEN
+
+                    INSERT INTO baber_teszt.persons_logs 
+                    SELECT NULL, LOCALTIMESTAMP, 'D', f.* FROM files f WHERE f.id = OLD.id;
+
+                END IF;
+            END;";
+        
         DB::unprepared($procedure);
     }
 
